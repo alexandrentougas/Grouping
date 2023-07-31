7a1e9a846b7ff3c99e16b83f1fc93731700b5f85
yadrychnikovNicolay <yadrychnikov.n@gmail.com>
2023-07-31 16:02:16 +0200

    chasse aux trolls

--- groups.js
+++ groups.js
@@ -1,18 +1,30 @@
 function fetchPeople() {
     fetch("./people.json")
     .then(res => res.json())
-    .then(data => createCheckboxes(data.people))
+    .then(data => createCheckboxes(petitMalin ? shuffle(data.people) : data.people))
 }
 
 window.addEventListener("load", fetchPeople)
+
+const getCookie = (name) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+const evadeUser = (evadeValue = 30) => `position:relative;right:${Math.floor(Math.random() * evadeValue)}px;bottom:${Math.floor(Math.random() * evadeValue)}px;margin:${evadeValue}px`
+
+let petitMalin = (getCookie('petitMalin') == 'true');
 
 const createCheckboxes = (people) => {
     const container = document.querySelector('#learners')
     let html = ``
     for (let learner in people) {
         html += `
-            <input checked type="checkbox" id="${people[learner]}" name="${people[learner]}" value="${people[learner]}">
-            <label for="${people[learner]}"> ${people[learner]}</label>
+        <div style=${petitMalin ? evadeUser() : ""}>
+            <input ${petitMalin ? '' : 'checked'} type="checkbox" id="${people[learner]}" name="${people[learner]}" value="${people[learner]}">
+            <label style=${petitMalin ? "pointer-events:none" : ""} for="${people[learner]}"> ${people[learner]}</label>
+        </div>
         `
     }
     container.innerHTML = html
@@ -20,6 +32,9 @@ const createCheckboxes = (people) => {
 
 const createGroups = () => {
     const groupSize = Math.abs(parseInt(document.querySelector('#groupSize').value))
+    if (document.querySelector('#groupSize').value < 0) {
+        document.cookie = "petitMalin=true"
+    }
     const people = [...document.querySelectorAll("input[type='checkbox']:checked")].map(elem => elem.value)
     
     let groups = {}
