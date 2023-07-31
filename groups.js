dc7c23a82b864e57d7fc6ecb1b1f1096d53c0a0e
yadrychnikovNicolay <yadrychnikov.n@gmail.com>
2023-07-31 16:30:37 +0200

    fix 0 and small refactor

--- groups.js
+++ groups.js
@@ -1,4 +1,4 @@
-function fetchPeople() {
+const fetchPeople = () => {
     fetch("./people.json")
     .then(res => res.json())
     .then(data => createCheckboxes(petitMalin ? shuffle(data.people) : data.people))
@@ -13,8 +13,7 @@ const getCookie = (name) => {
 }
 
 const evadeUser = (evadeValue = 30) => `position:relative;right:${Math.floor(Math.random() * evadeValue)}px;bottom:${Math.floor(Math.random() * evadeValue)}px;margin:${evadeValue}px`
-
-let petitMalin = (getCookie('petitMalin') == 'true');
+const petitMalin = getCookie('petitMalin') == 'true';
 
 const createCheckboxes = (people) => {
     const container = document.querySelector('#learners')
@@ -32,15 +31,17 @@ const createCheckboxes = (people) => {
 
 const createGroups = () => {
     const groupSize = Math.abs(parseInt(document.querySelector('#groupSize').value))
+
     if (document.querySelector('#groupSize').value < 0) {
         document.cookie = "petitMalin=true"
     }
-    const people = [...document.querySelectorAll("input[type='checkbox']:checked")].map(elem => elem.value)
     
+    const people = [...document.querySelectorAll("input[type='checkbox']:checked")].map(elem => elem.value)
     let groups = {}
     const shuffledPeople = shuffle(people);
+
     try {
-        for (let i = 0, j = 1; i < shuffledPeople.length; i = i + groupSize, j++ ) {
+        for (let i = 0, j = 1; i < shuffledPeople.length && i != 0; i = i + groupSize, j++ ) {
             if (shuffledPeople.slice(i, i + groupSize).length < groupSize - 1) {
                 for (let k = 0; k < shuffledPeople.slice(i, i + groupSize).length; k++) {
                     groups["Group " + (k + 1)].push(shuffledPeople.slice(i, i + groupSize)[k])
@@ -72,18 +73,11 @@ const  displayGroups = (groups) => {
 
 const shuffle = (array) => {
     let currentIndex = array.length,  randomIndex;
-
-    // While there remain elements to shuffle.
     while (currentIndex != 0) {
-
-        // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
-
-        // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
-
     return array;
 }
